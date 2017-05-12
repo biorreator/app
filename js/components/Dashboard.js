48cee4fe67de67917fb42b88a339a388a615f4bb
Marco William <marcowilliamdf@gmail.com>
2017-05-12 17:17:28 -0300

    Create dashboard component
    Signed-off-by: Daniel Henrique <danielhmarinho@gmail.com>

--- js/components/Dashboard.js
+++ js/components/Dashboard.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Modal, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Container, Content, Footer, FooterTab, Item, Label, Text, ListItem, Input,Button, List} from 'native-base';
+import Navigation from './Navigation';
 
 export default class Dashboard extends Component {
+
+  state = {
+    modalVisible: false,
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render() {
             return (
               <Container>
                 <Content>
-                    <List>
+                    <List style={{marginTop: 50}}>
                         <ListItem >
-                            <Text>{this.props.title}</Text>
+                            <Text>Densidade: {this.props.density}</Text>
                         </ListItem>
                         <ListItem>
-                            <Text>Nathaniel Clyne</Text>
+                            <Text>Ph:</Text>
                         </ListItem>
                         <ListItem>
-                            <Text>{this.props.title}</Text>
+                            <Text>Brix:</Text>
                         </ListItem>
                     </List>
+                    <Button style={{marginTop: 60, marginLeft: 120}} onPress={() => {
+                       this.setModalVisible(true)
+                     }}>
+                        <Text>Aumentar Grau Brix</Text>
+                    </Button>
+                    <Modal
+                      animationType={"fade"}
+                      transparent={false}
+                      visible={this.state.modalVisible}
+                      onRequestClose={() => {}}
+                      >
+                       <View style={{marginTop: 22}}>
+                        <View>
+                          <Text style={{marginLeft: 120}}>Quantos Grau Brix?</Text>
+                          <Item regular>
+                            <Input placeholder='Graus'/>
+                          </Item>
+                          <Button style={{marginTop: 60, marginLeft: 150}} onPress={() => {
+                            this.setModalVisible(!this.state.modalVisible)
+                          }}>
+                              <Text>Finalizar</Text>
+                          </Button>
+                        </View>
+                       </View>
+                      </Modal>
                 </Content>
+                <Footer>
+                <Navigation />
+                </Footer>
             </Container>
+
             );
         }
     }
